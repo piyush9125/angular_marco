e741f105af321d98ad222a2b69e637b74c897886
piyush9125 <pdpatil9125@gmail.com>
2025-03-05 21:54:07 +0530

    first commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  constructor(private userservice:UserService,private router:Router){}  
+ 
+  user={
+    username: '',
+    password: ''
+  };
+
+  login(){
+    this.userservice.login(this.user).subscribe((res)=>{
+      if(res!=null){
+        if(res.role==='Admin'){
+        this.router.navigateByUrl('admin-dashboard')
+        }else{
+          this.router.navigateByUrl('faculty-dashboard')
+        }
+      }else{
+        console.log("invalid credientials !")
+      }
+      
+    })
+  }
+
+}
